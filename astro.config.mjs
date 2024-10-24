a46f9472d36e951d2ce98b0b9a120306775afdef
adena <adena@detik.com>
2024-10-24 13:01:10 +0700

    modify: config astro, postcss and tailwind format

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,21 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  compressHTML: false,
+  vite: {
+    build: {
+      cssCodeSplit: false,
+    },
+  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
   ],
+  build: {
+    format: "file",
+    assets: "assets", //Change to your project name
+  },
+
+  output: "static",
 });
