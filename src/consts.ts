9e526a977c1ffe09fad9ecf0cbe14dc274772216
adena <adena@detik.com>
2024-10-24 12:27:18 +0700

    add files

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+// import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+
+export const SITE_TITLE = "Starter Pack Astro Sites"
+export const SITE_DESCRIPTION = "Just a starter pack for astro"
+export const BASE_URL = import.meta.env.BASE_URL
